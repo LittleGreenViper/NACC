aba8fede7bc4bf55095fd3289e5ab72f4e8b7926
Chris Marshall <chris@riftvalleysoftware.com>
2025-12-02 07:31:21 -0500

    I juiced up the datepicker screen.

--- Sources/SwiftUI/NACC_MainContentView.swift
+++ Sources/SwiftUI/NACC_MainContentView.swift
@@ -24,10 +24,100 @@ import LGV_UICleantime
 import RVS_Generic_Swift_Toolbox
 
 /* ###################################################################################################################################### */
+// MARK: - Date Picker View -
+/* ###################################################################################################################################### */
+/**
+ This is a view that we use to allow the user to select a new cleandate.
+ 
+ It shows a standard date picker (calendar-style), along with a "Today" button (sets the calendar to today), and a "DONE" button.
+ 
+ If the user has changed the date since invoking the screen, a "CANCEL" button also appears, allowing the user to discard any changes.
+ */
+struct PickerPopoverContent: View {
+    /* ################################################################## */
+    /**
+     This denotes the padding above the cleandate picker.
+     */
+    private static let _cleandatePickerTitlePaddingInDisplayUnits = 20.0
+    
+    /* ################################################################## */
+    /**
+     This stores the initial date, upon invoking the sheet/popover.
+     */
+    private static var _originalDate: Date = .now
+
+    /* ################################################################## */
+    /**
+     This is used to dismiss the modal.
+     */
+    @Environment(\.dismiss) private var _dismiss
+
+    /* ################################################################## */
+    /**
+     This connects to the main selected date.
+     */
+    @Binding var selectedDate: Date
+
+    /* ################################################################## */
+    /**
+     This returns the DatePicker screen.
+     */
+    var body: some View {
+        AppBackground {
+            VStack {
+                Text("SLUG-CLEANDATE-PICKER-TITLE".localizedVariant)
+                    .font(.largeTitle)
+                    .padding(.top, Self._cleandatePickerTitlePaddingInDisplayUnits)
+                    .padding(.horizontal, Self._cleandatePickerTitlePaddingInDisplayUnits)
+
+                Button("SLUG-TODAY".localizedVariant) {
+                    self.selectedDate = Date()
+                }
+                .disabled(Calendar.current.isDate(self.selectedDate, inSameDayAs: .now))
+                .padding(0)
+
+                DatePicker("Clean Date",
+                           selection: self.$selectedDate,
+                           in: ...Date(),
+                           displayedComponents: [.date]
+                )
+                .datePickerStyle(.graphical)
+                
+                HStack {
+                    if self.selectedDate != Self._originalDate {
+                        Button("SLUG-CANCEL".localizedVariant) {
+                            self.selectedDate = Self._originalDate
+                            self._dismiss()
+                        }
+                        .buttonStyle(.bordered)
+                        .frame(maxWidth: .infinity)
+                    }
+                    
+                    Button("SLUG-DONE".localizedVariant) {
+                        self._dismiss()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+        }
+        .onAppear { Self._originalDate = self.selectedDate }
+    }
+}
+
+/* ###################################################################################################################################### */
 // MARK: - Main App View -
 /* ###################################################################################################################################### */
 /**
  The initial app container View.
+ 
+ This is the principal view of the app. It is displayed upon startup.
+ 
+ It contains a textual report of the user's set cleantime, and the user can tap the report to change their cleandate.
+ 
+ Below the report, is an image, showing the last keytag (for under a year), or medallion earned by the user, based on their cleandate.
+ 
+ At the top, is the app logo. Tapping on it (or the keytag/medallion logo) will show another screen, with images displaying multiple keytags or medalliuons.
  */
 struct NACC_MainContentView: View {
     /* ################################################################## */
@@ -35,12 +125,6 @@ struct NACC_MainContentView: View {
      This is the string that displays the "cleantime report."
      */
     private let _reportString = LGV_UICleantimeDateReportString()
-    
-    /* ################################################################## */
-    /**
-     This denotes the padding above the cleandate picker.
-     */
-    private static let _cleandatePickerTitlePaddingInDisplayUnits = 20.0
     
     /* ################################################################## */
     /**
@@ -107,6 +191,24 @@ struct NACC_MainContentView: View {
      If true, then the NavigationStack will bring in the info screen.
      */
     @State private var _showInfo = false
+    
+    /* ################################################################## */
+    /**
+     This returns the contents of the textual report.
+     */
+    private var _report: String {
+        let calculator = LGV_CleantimeDateCalc(startDate: self._selectedDate).cleanTime
+
+        if calculator.isOneDayOrMore {
+            return self._reportString
+                .naCleantimeText(beginDate: self._selectedDate,
+                                 endDate: .now
+                )?
+                .localizedVariant ?? "ERROR"
+        } else {
+            return "SLUG-CLEANDATE-PICKER-TITLE".localizedVariant
+        }
+    }
 
     /* ################################################################## */
     /**
@@ -117,6 +219,7 @@ struct NACC_MainContentView: View {
         NavigationStack {
             AppBackground(alignment: .center) {
                 VStack {
+                    // This is the top logo. If the user has thirty days or more, tapping on it will bring in the results screen.
                     if LGV_CleantimeDateCalc(startDate: self._selectedDate).cleanTime.isThirtyDaysOrMore {
                         Button {
                             self._prefs.lastSelectedTabIndex = TabInexes.keytagArray.rawValue
@@ -135,44 +238,32 @@ struct NACC_MainContentView: View {
                             .frame(width: Self._iconSizeInDisplayUnits)
                     }
                     
+                    // If the user has not specified a cleandate, this text item displays a message to that effect.
                     if !LGV_CleantimeDateCalc(startDate: self._selectedDate).cleanTime.isOneDayOrMore {
-                        Text("NO CLEANDATE YET")
+                        Text("SLUG-NO-CLEANDATE-YET".localizedVariant)
                     }
                     
-                    if let report = self._reportString.naCleantimeText(beginDate: self._selectedDate,
-                                                                       endDate: .now
-                    )?.localizedVariant,
-                       !report.isEmpty {
-                        Text(report)
-                            .textSelection(.enabled)
-                                .padding(Self._buttonPaddingInDisplayUnits)
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .foregroundStyle(!LGV_CleantimeDateCalc(startDate: _selectedDate).cleanTime.isOneDayOrMore ? Color("SelectionTintColor") : .primary)
-                                .background(.thickMaterial, in: Capsule())
-                                .contentShape(Rectangle())
-                            .onTapGesture {
-                                _showingPicker = true
-                            }
-                            .popover(isPresented: $_showingPicker) {
-                                AppBackground {
-                                    Text("SLUG-CLEANDATE-PICKER-TITLE".localizedVariant)
-                                        .font(.largeTitle)
-                                        .padding(.top, Self._cleandatePickerTitlePaddingInDisplayUnits)
-                                        .padding([.leading, .trailing], Self._cleandatePickerTitlePaddingInDisplayUnits)
-                                    
-                                    DatePicker(
-                                        "Clean Date",
-                                        selection: self.$_selectedDate,
-                                        displayedComponents: [.date]
-                                    )
-                                    .datePickerStyle(.graphical)
-                                    .padding()
-                                }
-                            }
-                            .accessibilityAddTraits(.isButton)
-                            .accessibilityHint("SLUG-ACC-REPORT-BUTTON".localizedVariant)
-                    }
+                    // If there has been a cleandate set, the following displays a text item, with the textual report. If no cleandate, then it prompts the user to set one.
+                    // Tapping on the report brings up a date picker, allowing the user to set/change the cleandate.
+                    // The text is surrounded by a capsule, indicating that it can be tapped. If no date is set, the color of the text is a button color.
+                    Text(self._report)
+                        .textSelection(.enabled)
+                            .padding(Self._buttonPaddingInDisplayUnits)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundStyle(!LGV_CleantimeDateCalc(startDate: self._selectedDate).cleanTime.isOneDayOrMore ? Color("SelectionTintColor") : .primary)
+                            .background(.thickMaterial, in: Capsule())
+                            .contentShape(Rectangle())
+                        .onTapGesture {
+                            self._showingPicker = true
+                        }
+                        .popover(isPresented: $_showingPicker) {
+                            PickerPopoverContent(selectedDate: $_selectedDate)
+                        }
+                        .accessibilityAddTraits(.isButton)
+                        .accessibilityHint("SLUG-ACC-REPORT-BUTTON".localizedVariant)
                     
+                    // If the user has set a cleandate, then the following is an image, with the user's last earned keytag (under a year), or medallion.
+                    // Tapping on the image brings in the results screen.
                     if let image = self._displayedImage {
                         if LGV_CleantimeDateCalc(startDate: self._selectedDate).cleanTime.isThirtyDaysOrMore {
                             Button {
